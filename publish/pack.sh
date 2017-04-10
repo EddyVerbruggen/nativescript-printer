036992bf002af96db6eea475aabd030ed7eadb89
Eddy Verbruggen <eddyverbruggen@gmail.com>
2017-04-10 19:33:11 +0200

    README & lICENSE

--- publish/pack.sh
+++ publish/pack.sh
@@ -20,6 +20,11 @@ pack() {
     echo 'Copying src...'
     node_modules/.bin/ncp "$SOURCE_DIR" "$TO_SOURCE_DIR"
 
+    # copy README & LICENSE to src
+    echo 'Copying README and LICENSE to /src...'
+    node_modules/.bin/ncp "$ROOT_DIR"/LICENSE "$TO_SOURCE_DIR"/LICENSE
+    node_modules/.bin/ncp "$ROOT_DIR"/README.md "$TO_SOURCE_DIR"/README.md
+
     # compile package and copy files required by npm
     echo 'Building /src...'
     cd "$TO_SOURCE_DIR"
